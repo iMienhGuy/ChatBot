c3215fa9569240b950ae925f95c1b8333dbe44ce
Peter Le <Peterle3@csus.edu>
2017-01-03 18:27:06 -0800

    first commit

--- index.php
+++ index.php
@@ -13,12 +13,16 @@
 			</div>
 
 			<div id="chatbox"></div>
-
-			<form name="message" action="">
+			<div id ="test">
+			<form name="message" id = "chat" action="">
+			
 				<input name="usermsg" type="text" id="usermsg" size="63"/>
 				<input name="submitmsg" type="submit" id="submitmsg" value="Send"/>
+			
 			</form>
-		</div>
+			</div>
+			</div>
+	
 
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
 		<script src="main.js"></script>
