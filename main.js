e7281e8051418e6c7bffbd54b4eb28b5f9ed0cbc
Peter Le <Peterle3@csus.edu>
2017-01-09 22:16:48 -0800

    In Progress CSS animation

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 $(document).ready(function(){
-    $("#first")
-      .fadeIn(1500)
-      .animate({top : "-=500"}, {duration: 1500, queue: false});
-      $("#first").fadeOut(1500);
-    $("#click")
-    	.fadeIn(3000)
-    	.animate({top : "-=550"}, {duration: 1500, queue: false});
-	   
 
-    document.getElementById("first").innerHTML = "My name is Chat Box";
-    $("#first").fadeIn(1500);
+    //$("#first").animate({top : "-=500"}, {duration: 1500, queue: false});
+  //    $("#first").fadeOut(1500);
+    //$("#user").animate({top : "-=550"}, {duration: 1500, queue: false});
+	   //  $("#first").fadeOut(1500);
+       //  $("#first").fadeIn(1500);
+
+    /*document.getElementById("first").innerHTML = "My name is Chat Box";
+    $("#first").fadeIn(1500); 
     $("#user").fadeIn(5000);
+    */
 
+    setTimeout(function() {
+       //  var first = document.getElementById("first");
+        // first.style.display = "none";
+        var second = document.getElementById("second");
+        second.style.visibility = "visible";
+
+
+    }, 5000)
+      
+    
       $('#submit').click(function() {
-  //  $(this).next().toggle();
   		$('#user').hide();
    		$('h1').fadeIn(1000);
    		$('.smallChatBox').fadeIn(1000);
