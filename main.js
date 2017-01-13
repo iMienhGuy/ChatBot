3fc1d0f2415986acabf82eae7b2c1dae5524b6f4
iMienhGuy <stancslee@gmail.com>
2017-01-13 13:37:37 -0800

    Opening animations complete. Known Bug: Greeting text not centered

--- main.js
+++ main.js
@@ -3,27 +3,25 @@ $(document).ready(function(){
 	$("#first")
 		.fadeIn(1500)
 		.animate({top : "-=500"}, {duration: 1500, queue: false});
-	$("#first")
-		.fadeOut(1500);
-
-	/*
-	$("#second").setTimeout(function() {
-		$("#second").fadeIn(1500);
-	}, 1000);
-	*/
-
-	$("#user").fadeIn();
-
-	/*
-	
-	 $("#first").animate({
-		opacity: 1,
-		top: "-=500"		
-	}, 1500, function() {
-		// Animation Complete
+	$("#first").fadeOut(1500, function(){
+		$("#second").fadeIn(1500, function(){
+			$("#second").fadeOut(1500, function(){
+				$("#third").fadeIn(1500, function(){
+					$("#name").fadeIn();
+					$("#name").animate({height:"200px", width:"350px"});
+					$("#name").children("p").animate({fontSize:"25px"}, function(){
+						$("#user").fadeIn(1500);
+					});
+				})
+			});
+		});
 	});
-	*/
-	
 
+	$("#submit").click(function(){
+		$("#third").fadeOut(1500);
+		$("#name").fadeOut(1500, function(){
+			$("#container").fadeIn(1500);
+		});
+	})
 
 });
